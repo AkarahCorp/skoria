94cfd2189304004980ae2b9fa6af299a10820bc9
akarahdev <endisticbutitsforgit@gmail.com>
2025-04-04 10:20:24 -0400

    feat: add some javadocs to Nullable and Result

--- src/main/java/dev/akarah/skoria/util/Nullable.java
+++ src/main/java/dev/akarah/skoria/util/Nullable.java
@@ -2,13 +2,28 @@ package dev.akarah.skoria.util;
 
 import java.util.function.Function;
 
+/**
+ * `Nullable` is a safe interface that allows for empty values, while requiring checks.
+ */
 public sealed interface Nullable<T> {
+    /**
+     * Represents a value that is present.
+     */
     record Present<T>(T value) implements Nullable<T> {
     }
 
+    /**
+     * Represents a value that does not exist.
+     */
     record Empty<T>() implements Nullable<T> {
     }
 
+    /**
+     * Creates a new Nullable value.
+     * @param <T> The type of the Nullable.
+     * @param value If null, a Nullable.Empty is returned. Otherwise, a Nullable.Present wrapping the value is returned.
+     * @return The returned value, either Nullable.Empty or Nullable.Present.
+     */
     static <T> Nullable<T> of(T value) {
         if (value == null) {
             return new Empty<>();
@@ -16,15 +31,31 @@ public sealed interface Nullable<T> {
         return new Present<>(value);
     }
 
+    /**
+     * Creates a new Nullable value out of a non-null value.
+     * @param <T> The type of the Nullable.
+     * @param value If null, an exception is thrown.
+     * @return The non-null value wrapped in a Nullable.Present.
+     */
     static <T> Nullable<T> ofNonNull(T value) {
         assert value != null;
         return new Present<>(value);
     }
 
+     /**
+     * Creates a new empty Nullable value.
+     * @param <T> The type of the Nullable.
+     * @return An Nullable.Empty instance.
+     */
     static <T> Nullable<T> ofNull() {
         return new Empty<>();
     }
 
+    /**
+     * Maps a Nullable value's contents with a function. The function will be evaluated only if the Nullable instance is Present.
+     * @param function The function to map with.
+     * @return A new Nullable instance. Empty if the provided Nullable is Empty, otherwise it is Present with the new value.
+     */
     default Nullable<T> map(Function<T, T> function) {
         switch (this) {
             case Nullable.Present(T value) -> {
@@ -36,6 +67,14 @@ public sealed interface Nullable<T> {
         }
     }
 
+    /**
+     * Flat maps a Nullable value.
+     * If a Nullable.Empty is passed in, nothing happens.
+     * If a Nullable.Present is passed in, and the function returns a Nullable.Present, the new value will be returned.
+     * If a Nullable.Present is passed in, and the function returns a Nullable.Empty, a Nullable.Empty is returned.
+     * @param function The function to perform the flat map with.
+     * @return The flatmapped Nullable value.
+     */
     default Nullable<T> flatMap(Function<T, Nullable<T>> function) {
         switch (this) {
             case Nullable.Present(T value) -> {
@@ -47,6 +86,11 @@ public sealed interface Nullable<T> {
         }
     }
 
+    /**
+     * Returns the value inside the Nullable.Present, throwing a NullPointerException otherwise.
+     * @throws NullPointerException If the provided Nullable is Empty.
+     * @return
+     */
     default T unwrap() {
         switch (this) {
             case Nullable.Present(T value) -> {
@@ -58,10 +102,16 @@ public sealed interface Nullable<T> {
         }
     }
 
+    /**
+     * @return True, if this is an instance of Nullable.Present;
+     */
     default boolean isPresent() {
         return this instanceof Nullable.Present;
     }
 
+    /**
+     * @return True, if this is an instance of Nullable.Empty;
+     */
     default boolean isNull() {
         return this instanceof Nullable.Empty;
     }
--- src/main/java/dev/akarah/skoria/util/Result.java
+++ src/main/java/dev/akarah/skoria/util/Result.java
@@ -3,24 +3,54 @@ package dev.akarah.skoria.util;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Result is a safe interface for handling errors.
+ */
 public sealed interface Result<T, E> {
 
+    /**
+     * Represents a successful result.
+     */
     record Success<T, E>(T value) implements Result<T, E> {
 
     }
 
+    /**
+     * Indicates that an error has occured.
+     */
     record Error<T, E>(E value) implements Result<T, E> {
 
     }
 
+    /**
+     * Creates a new successful instance of the Result type.
+     * @param <T> The type of the successful value.
+     * @param <E> The type of the error value.
+     * @param value The value to pass as a success.
+     * @return A successful Result type instance with the provided value.
+     */
     static <T, E> Result<T, E> success(T value) {
         return new Result.Success<>(value);
     }
 
+    /**
+     * Creates a new error instance of the Result type.
+     * @param <T> The type of the successful value.
+     * @param <E> The type of the error value.
+     * @param value The value to pass as an error.
+     * @return A errored Result type instance with the provided value.
+     */
     static <T, E> Result<T, E> error(E value) {
         return new Result.Error<>(value);
     }
 
+     /**
+     * Creates a new instance of the Result type, based on whether the provided function throws an exception.
+     * @param <T> The type of the successful value.
+     * @param <E> The type of the error value.
+     * @param value The function to run with checks.
+     * @return Returns successful if no exception was thrown. If an exception was thrown, an error is returned with the exception.
+     */
     static <T> Result<T, Exception> fromExceptional(Supplier<T> value) {
         try {
             return Result.success(value.get());
@@ -29,14 +59,25 @@ public sealed interface Result<T, E> {
         }
     }
 
+    /**
+     * @return True if this Result value is successful.
+     */
     default boolean isSuccess() {
         return this instanceof Result.Success;
     }
 
+    /**
+     * @return True if this Result value is an error.
+     */
     default boolean isError() {
         return this instanceof Result.Error;
     }
 
+    /**
+     * Maps a Result's value with the function.
+     * @param function The function to map the Result.Success's value with
+     * @return A new Result instance. Returns Success if a successful Result was passed in, otherwise returns an Error.
+     */
     default Result<T, E> map(Function<T, T> function) {
         switch (this) {
             case Success<T, E>(T value) -> {
@@ -48,6 +89,11 @@ public sealed interface Result<T, E> {
         }
     }
 
+    /**
+     * Maps a Result's error value with the function.
+     * @param function The function to map the Result.Error's value with
+     * @return A new Result instance. Returns Error if a errorful Result was passed in, otherwise returns an Success.
+     */
     default Result<T, E> mapErr(Function<E, E> function) {
         switch (this) {
             case Success<T, E>(T value) -> {
@@ -59,6 +105,11 @@ public sealed interface Result<T, E> {
         }
     }
 
+    /**
+     * Attempts to unwrap the value inside the Success case.
+     * @throws NullPointerException If a Success.Error was passed in.
+     * @return The value inside the Result.Success.
+     */
     default T unwrap() {
         switch (this) {
             case Success<T, E> ok -> {
@@ -70,6 +121,11 @@ public sealed interface Result<T, E> {
         }
     }
 
+    /**
+     * Attempts to unwrap the value inside the Error case.
+     * @throws NullPointerException If a Success.Success was passed in.
+     * @return The value inside the Result.Error.
+     */
     default E unwrapErr() {
         switch (this) {
             case Success<T, E> _ -> {
@@ -81,6 +137,10 @@ public sealed interface Result<T, E> {
         }
     }
 
+    /**
+     * @param value The default value to use.
+     * @return Unwraps the value inside the succcessful result. If this is an error, return the default value provided.
+     */
     default T unwrapOr(T value) {
         switch (this) {
             case Success<T, E>(T resultValue) -> {
@@ -92,6 +152,10 @@ public sealed interface Result<T, E> {
         }
     }
 
+    /**
+     * @param value The default value to use, obtained through the supplier.
+     * @return Unwraps the value inside the succcessful result. If this is an error, return the default value provided by the supplier.
+     */
     default T unwrapOr(Supplier<T> value) {
         switch (this) {
             case Success<T, E> ok -> {
